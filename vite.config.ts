c35aafed428533e81b475492a18f62d5940a7e17
salvatorelamarca1 <paslamarca@gmail.com>
2026-01-02 20:08:00 -0500

    Updating the path to repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/realYM2025/",
+  base: "/Yasmin-Month/",
   build: {
     sourcemap: true,
   }
